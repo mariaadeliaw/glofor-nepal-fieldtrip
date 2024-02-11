a23af4c0680b2852cc82ebd25029b039dc1d7eee
mariaadeliaw <mariaadeliawidijanto@gmail.com>
2024-02-11 13:12:07 +0545

    change df format

--- r/importancevalue.R
+++ r/importancevalue.R
@@ -3,9 +3,11 @@ library(dplyr)
 library(readxl)
 
 # import data from local
+# data has been cleaned manually in excel, separated into different sheet for different year obs
+# empty rows from missing observation in each years had been deleted manually too
 psp_data <- "data/Gyachhowk2016-2019extractForGLOFOR-EDIT.xlsx"
 
-# Assuming "PlotNo" column contains the Plot numbers
+# plot inside group's study extent
 plot_numbers <- c(90005, 90010, 90001, 90004, 90006, 90028, 90011)
 
 # 2016 ivi calc
@@ -21,7 +23,12 @@ ivi_2016 <- psp_data_2016 %>%
                   species = "species",
                   count = "count",
                   basal = "ba",
-                  factor = "")
+                  factor = "") %>% 
+  as.data.frame()
+ivi_2016$species <- rownames(ivi_2016)
+ivi_2016 <- ivi_2016 %>% relocate(species) %>% as_tibble()
+
+
 # 2019 ivi calc
 psp_data_2019 <- psp_data %>% 
   read_excel(sheet = "data_2019") %>% 
@@ -35,4 +42,7 @@ ivi_2019 <- psp_data_2019 %>%
                   species = "species",
                   count = "count",
                   basal = "ba",
-                  factor = "")
+                  factor = "") %>% 
+  as.data.frame()
+ivi_2019$species <- rownames(ivi_2019)
+ivi_2019 <- ivi_2019 %>% relocate(species) %>% as_tibble()
